49432fcaa51e401a2669d675cd1026fbcd53f2f5
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-05-09 13:17:35 +0545

    javscirpt 2.0 starts from here

---
+++ first.js
@@ -0,0 +1,12 @@
+const alertfirst = alert("this is my first alert")
+
+console.log(alertfirst)
+
+const firstConfirm = confirm("This is my first confirm")
+
+console.log(firstConfirm);
+
+
+const firstPrompt = prompt("this is my first prompt")
+
+console.log(firstPrompt);
