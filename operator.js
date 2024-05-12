73e9c6e911f1861dec82eff51923b584d976bb57
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-05-12 17:01:20 +0545

    arithimatic operator

---
+++ operator.js
@@ -0,0 +1,12 @@
+let x = 3
+let y = 4
+let sum = x + y
+console.log(sum);
+let diff = x - y
+console.log(diff);
+let multi = x * y
+console.log(multi);
+let division = x / y
+console.log(division);
+let modulus = x % y
+console.log(modulus);
